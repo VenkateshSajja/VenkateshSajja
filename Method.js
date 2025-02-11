08011e2414a7cc79b367adbc94a531cb46182caf
VenkateshSajja <20058351@mydbs.ie>
2025-02-11 13:51:42 +0000

    Method.js

---
+++ Method.js
@@ -0,0 +1,37 @@
+// Apart from storing data, you can attach functionality to an object whereas maps are restricted to just storing data. 
+
+// Create an object with data and a method
+const person = {
+    name: 'Full Name',
+    age: 25,
+    city: 'Dublin',
+
+    getInfo: function() {
+        return `${this.name}, ${this.age} years old, lives in ${this.city}.`;
+    }
+};
+
+console.log("Object Example:");
+console.log(person.name); 
+console.log(person.age);  
+
+console.log(person.getInfo()); 
+
+
+// Create a Map to store data
+const personMap = new Map();
+personMap.set('name', 'Full Name');
+personMap.set('age', 25);
+personMap.set('city', 'Dundalk');
+
+
+console.log("\nMap Example:");
+console.log(personMap.get('name')); 
+console.log(personMap.get('age'));  
+
+
+personMap.set('getInfo', function() {
+    return `${this.get('name')}, ${this.get('age')} years old, lives in ${this.get('city')}.`;
+});
+
+console.log(personMap.get('getInfo'));
